abcbef1348b6e6a91451c61079c5b21836a30a9e
lucian606 <lucian.iliescu@stud.acs.upb.ro>
2022-02-08 03:43:02 +0200

    Implement login functionality

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,30 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { useRef, useState } from "react";
 
 export default function LoginPage() {
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const { login } = useAuth();
+    const [error, setError] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            setError('');
+            await login(emailRef.current.value, passwordRef.current.value);
+            setError('Successfully logged in');
+        } catch (error) {
+            if (error.code == 'auth/wrong-password') {
+                setError('Wrong password');
+            } else {
+                setError('Failed to login');
+            }
+        }
+    }
+    
     return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center">
         <div className="max-w-md w-full mx-auto">
@@ -8,14 +32,22 @@ export default function LoginPage() {
             <div className="text-3xl font-bold text-gray-900 mt-2 text-center">Studdy Buddy</div>
         </div>
         <div className="max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl w-full mx-auto mt-4 bg-white p-8 border border-gray-300">
-            <form action="" className="space-y-6">
+            <form onSubmit={handleSubmit} className="space-y-6">
+                {error !== '' &&
+                    <div class="bg-red-100 border border-red-400 text-red-700 rounded relative flex justify-center" role="alert">
+                        <div>
+                            <strong class="text-md xl:text-xl 2xl:text-2xl font-bold">Error: </strong>
+                            <div class="text-md xl:text-xl 2xl:text-2xl sm:inline">{error}</div>
+                        </div>
+                    </div>
+                }
                 <div>
                     <label className="text-sm xl:text-xl 2xl:text-2xl font-bold text-gray-900 block">Username</label>
-                    <input type="text" className="text-sm lg:text-lg xl:text-xl 2xl:text-2xl  w-full p-2 border border-gray-300 rounded mt-1" />
+                    <input ref={emailRef} type="email" className="text-sm lg:text-lg xl:text-xl 2xl:text-2xl  w-full p-2 border border-gray-300 rounded mt-1" />
                 </div>
                 <div>
                     <label className="text-sm xl:text-xl 2xl:text-2xl font-bold text-gray-900 block">Password</label>
-                    <input type="password" className="text-sm lg:text-lg xl:text-xl 2xl:text-2xl w-full p-2 border border-gray-300 rounded mt-1" />
+                    <input ref={passwordRef} type="password" className="text-sm lg:text-lg xl:text-xl 2xl:text-2xl w-full p-2 border border-gray-300 rounded mt-1" />
                 </div>
                 <div className="flex items-center justify-between">
                     <div className="flex items-center">
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -14,6 +14,10 @@ export function AuthProvider({ children }) {
         return auth.createUserWithEmailAndPassword(email, password);
     }
 
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password);
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user);
@@ -24,7 +28,8 @@ export function AuthProvider({ children }) {
 
     const value = {
         currentUser,
-        signup
+        signup,
+        login
     };
     
     return (
