3b31e8fa4a8bc18e0a8c24fb05d953cb2393daa7
lucian606 <lucian.iliescu@stud.acs.upb.ro>
2022-04-05 20:34:04 +0300

    Add logout + PDF View + style navbar

--- src/components/SubjectGeneratorPage.js
+++ src/components/SubjectGeneratorPage.js
@@ -18,6 +18,18 @@ export default function SubjectGeneratorPage(props, ref) {
     const subjectLinkRef = storage.refFromURL("gs://studybuddy-d5c95.appspot.com/Istorie_Uman.1.1.pdf");
     console.log(subjectLinkRef);
 
+    function toggleFirstSubj() {
+        setShowFirstSubj(!showFirstSubj);
+    }
+
+    function toggleSecondSubj() {
+        setShowSecondSubj(!showSecondSubj);
+    }
+
+    function toggleThirdSubj() {
+        setShowThirdSubj(!showThirdSubj);
+    }
+
     async function getSubject() {
         let subject = await subjectLinkRef.getDownloadURL();
         console.log(subject);
@@ -64,19 +76,43 @@ export default function SubjectGeneratorPage(props, ref) {
                     <div className="flex justify-center">
                         <a href={subjectUrl} target="_blank" className="text-sm md:text-md lg:text-lg xl:text-xl 2xl:text-2xl text-blue-500 hover:text-blue-700">Subiectul I</a>
                     </div>
-                    <div className="flex justify-center">
+                    <div className="max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl w-full mx-auto mt-4 bg-white p-8 border border-gray-300">
+                        <h2 id="accordion-collapse-heading-1">
+                            <button type="button" onClick={toggleFirstSubj} class="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                                <span>Arata subiectul I</span>
+                                <svg data-accordion-icon className={`w-6 h-6 ${showFirstSubj && 'rotate-180'} shrink-0`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </button>
+                        </h2>
+                    </div>
+                    <div className={`flex justify-center ${!showFirstSubj && 'hidden'}`}>
                         <Pdf url={subjectUrl}/>
                     </div>
                     <div className="flex justify-center">
                         <a href={subjectUrl} target="_blank" className="text-sm md:text-md lg:text-lg xl:text-xl 2xl:text-2xl text-blue-500 hover:text-blue-700">Subiectul al II-lea</a>
                     </div>
-                    <div className="flex justify-center">
+                    <div className="max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl w-full mx-auto mt-4 bg-white p-8 border border-gray-300">
+                        <h2 id="accordion-collapse-heading-1">
+                            <button type="button" onClick={toggleSecondSubj} class="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                                <span>Arata subiectul al II-lea</span>
+                                <svg data-accordion-icon className={`w-6 h-6 ${showSecondSubj && 'rotate-180'} shrink-0`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </button>
+                        </h2>
+                    </div>
+                    <div className={`flex justify-center ${!showSecondSubj && 'hidden'}`}>
                         <Pdf url={subjectUrl}/>
                     </div>
                     <div className="flex justify-center">
                         <a href={subjectUrl} target="_blank" className="text-sm md:text-md lg:text-lg xl:text-xl 2xl:text-2xl text-blue-500 hover:text-blue-700">Subiectul al III-lea</a>
                     </div>
-                    <div className="flex justify-center">
+                    <div className="max-w-sm md:max-w-md xl:max-w-xl 2xl:max-w-2xl w-full mx-auto mt-4 bg-white p-8 border border-gray-300">
+                        <h2 id="accordion-collapse-heading-1">
+                            <button type="button" onClick={toggleThirdSubj} class="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                                <span>Arata subiectul al III-lea</span>
+                                <svg data-accordion-icon className={`w-6 h-6 ${showThirdSubj && 'rotate-180'} shrink-0`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </button>
+                        </h2>
+                    </div>
+                    <div className={`flex justify-center ${!showThirdSubj && 'hidden'}`}>
                         <Pdf url={subjectUrl}/>
                     </div>
                 </div>
